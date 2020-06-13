07bac68ab7a499bac344d883d1d1f5a48f91620c
Gabe Bobbitt <bobbittgabriel@gmail.com>
2020-06-13 13:50:46 -0400

    Initial commit.

---
+++ bramblebee_description/env-hooks/50.bramblebee_description.sh
@@ -0,0 +1,1 @@
+export HUSKY_DESCRIPTION=$(catkin_find --first-only bramblebee_description urdf/bramblebee_description.urdf.xacro 2>/dev/null)
---
+++ bramblebee_gazebo/env-hooks/50.bramblebee_gazebo.sh
@@ -0,0 +1,1 @@
+export HUSKY_GAZEBO_DESCRIPTION=$(catkin_find --first-only bramblebee_gazebo urdf/bramblebee_description.gazebo.xacro 2>/dev/null)
